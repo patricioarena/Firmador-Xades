4b2eaab622286ef1e9f4e8e73e02a672c81b922c
Patricio Arena <parena@PARENA>
2019-11-01 14:52:06 -0300

    Funcionalidad de firma lista y verificacion de firma lista

---
+++ Demo/Demo/Model/ObjetoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Model
+{
+    public class ObjetoModel
+    {
+        public string Archivo { get; set; }
+        public string Extension { get; set; }
+    }
+}
