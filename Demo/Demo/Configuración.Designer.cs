b4f771c8143086f093caf2f912023cf74f6d5b99
Patricio Arena <parena@PARENA>
2019-11-13 15:48:42 -0300

    Actualizacion de interfaz desktop

--- Demo/Demo/Configuración.Designer.cs
+++ Demo/Demo/Configuración.Designer.cs
@@ -28,11 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Signature));
             this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -41,23 +54,27 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(368, 41);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Certificates";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Enter += new System.EventHandler(this.button1_Enter);
+            this.button1.Leave += new System.EventHandler(this.button1_Leave);
             // 
             // listView1
             // 
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listView1.FullRowSelect = true;
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(13, 59);
+            this.listView1.Location = new System.Drawing.Point(11, 3);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(368, 379);
+            this.listView1.Size = new System.Drawing.Size(368, 270);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ListView_KeyDown);
             this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ListView1MouseDoubleClick);
             // 
             // columnHeader1
@@ -70,19 +87,114 @@
             this.columnHeader2.Text = "Verify";
             this.columnHeader2.Width = 58;
             // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Gainsboro;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 47);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "     Versiòn: 1.0.0.0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 36);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form_Show);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(1, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(389, 291);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkBox3);
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(390, 273);
+            this.panel2.TabIndex = 6;
+            this.panel2.Visible = false;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(11, 78);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(201, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Buscar actualizaciones manualmente";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(11, 48);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(160, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Actualizaciones automáticas";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(106, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Inicio automático";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.listView1);
+            this.panel3.Location = new System.Drawing.Point(1, 8);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(390, 273);
+            this.panel3.TabIndex = 7;
+            this.panel3.Visible = false;
+            // 
             // Signature
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 450);
-            this.Controls.Add(this.listView1);
+            this.ClientSize = new System.Drawing.Size(391, 405);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Signature";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Signature";
-            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.TopMost = true;
             this.Load += new System.EventHandler(this.Configuración_Load);
+            this.Resize += new System.EventHandler(this.Form_Hide);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -93,6 +205,15 @@
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
--- Demo/Demo/Configuración.cs
+++ Demo/Demo/Configuración.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,68 +18,89 @@ namespace Demo
     public partial class Signature : Form
     {
         private readonly List<IDataNode> listDataNode = new List<IDataNode>();
+        private static string assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
         public Signature()
         {
             InitializeComponent();
+            panel1.Visible = true;
+            panel2.Visible = true;
+            panel3.Visible = false;
         }
 
         private void Configuración_Load(object sender, EventArgs e)
         {
-
-        }
-
-
-        private void ListView1MouseClick(object sender, MouseEventArgs e)
-        {
-
+            label1.Text = String.Format("     Version: {0}", assemblyVersion);
         }
 
         private void ListView1MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            ListView lv = (ListView)sender;
-            ListViewItem lvi = lv.GetItemAt(e.X, e.Y);
-            String Thumbprint = lv.SelectedItems[0].SubItems[2].Text;
-
-            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
-            store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
-            X509Certificate2Collection collection = (X509Certificate2Collection)store.Certificates;
-            X509Certificate2 certificate = collection.OfType<X509Certificate2>().Where(cert => cert.Thumbprint == Thumbprint).FirstOrDefault();
-            X509Certificate2UI.DisplayCertificate(certificate);
+            ListView listView = (ListView)sender;
+            ListViewItem item = listView.FocusedItem;
+            ViewCert(item);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            listDataNode.Clear();
-            ObtainModel();
-            listView1.Items.Clear();
-            listView1.Refresh();
-            for (int i = 0; i < listDataNode.Count; i++)
+            panel2.Visible = !panel2.Visible;
+            panel3.Visible = !panel3.Visible;
+            
+            if (panel2.Visible == true)
             {
-                IDataNode item = listDataNode[i];
-                ListViewItem listViewItem = new ListViewItem();
-                ListViewItem.ListViewSubItem subItem_flag = new ListViewItem.ListViewSubItem();
-                ListViewItem.ListViewSubItem subItem_Thumbprint = new ListViewItem.ListViewSubItem();
-                
-                listViewItem.Text = item.Name;
-                subItem_Thumbprint.Text = item.Thumbprint;
-                subItem_flag.Font = new Font(listViewItem.Font, FontStyle.Bold);
-                listViewItem.UseItemStyleForSubItems = false;
+                panel3.SendToBack();
+                panel2.BringToFront();
+                //LoadViewList();
+            }
+            if (panel2.Visible == false)
+            {
+                panel2.SendToBack();
+                panel3.BringToFront();
+                LoadViewList();
+            }
+            panel1.Refresh();
+            Console.WriteLine("cambie la prop visible del panel, puto");
+        }
 
-                if (item.isValid)
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+
+            //notifyIcon.Visible = true;
+            //this.MessagesOnChange();
+            //this.Hide();
+        }
+
+        private void Form_Hide(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon.Visible = true;
+                MessagesOnChange();
+                Hide();
+            }
+        }
+
+        private void Form_Show(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+
+        private void ListView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Space)
+            {
+                ListView listView = (ListView)sender;
+                for (int i = 0; i < listView.Items.Count; i++)
                 {
-                    subItem_flag.Text = "Valid";
-                    subItem_flag.ForeColor = Color.Green;
-                }
-                else
-                {
-                    subItem_flag.Text = "Invalid";
-                    subItem_flag.ForeColor = Color.Red;
+                    if (listView.SelectedIndices[0] == i)
+                    {
+                        ListViewItem item = listView.SelectedItems[0];
+                        ViewCert(item);
+                    }
                 }
 
-                listView1.Items.Add(listViewItem);
-                listViewItem.SubItems.Add(subItem_flag);
-                listViewItem.SubItems.Add(subItem_Thumbprint);
             }
         }
 
@@ -100,7 +123,76 @@ namespace Demo
 
         }
 
+        private void LoadViewList()
+        {
+            listDataNode.Clear();
+            ObtainModel();
+            listView1.Items.Clear();
+            for (int i = 0; i < listDataNode.Count; i++)
+            {
+                IDataNode item = listDataNode[i];
+                ListViewItem listViewItem = new ListViewItem();
+                ListViewItem.ListViewSubItem subItem_flag = new ListViewItem.ListViewSubItem();
+                ListViewItem.ListViewSubItem subItem_Thumbprint = new ListViewItem.ListViewSubItem();
 
+                listViewItem.Text = item.Name;
+                subItem_Thumbprint.Text = item.Thumbprint;
+                subItem_flag.Font = new Font(listViewItem.Font, FontStyle.Bold);
+                listViewItem.UseItemStyleForSubItems = false;
+
+                if (item.isValid)
+                {
+                    subItem_flag.Text = "Valid";
+                    subItem_flag.ForeColor = Color.Green;
+                }
+                else
+                {
+                    subItem_flag.Text = "Invalid";
+                    subItem_flag.ForeColor = Color.Red;
+                }
+
+                listView1.Items.Add(listViewItem);
+                listViewItem.SubItems.Add(subItem_flag);
+                listViewItem.SubItems.Add(subItem_Thumbprint);
+            }
+        }
+
+        private void ViewCert(ListViewItem item)
+        {
+            String Thumbprint = item.SubItems[2].Text;
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+            X509Certificate2Collection collection = (X509Certificate2Collection)store.Certificates;
+            X509Certificate2 certificate = collection.OfType<X509Certificate2>().Where(cert => cert.Thumbprint == Thumbprint).FirstOrDefault();
+            X509Certificate2UI.DisplayCertificate(certificate);
+        }
+
+        private void MessagesOnChange()
+        {
+            //if (Network.StatusPrincipalInterface().Equals(true))
+            //{
+            //    this.notifyIcon.BalloonTipText = "« Adaptador de Red: Habilitado »";
+            //    this.notifyIcon.Text = "« Adaptador de Red: Habilitado »";
+            //    notifyIcon.ShowBalloonTip(500);
+
+            //}
+            //else
+            //{
+            //    this.notifyIcon.BalloonTipText = "« Adaptador de Red: Deshabilitado »";
+            //    this.notifyIcon.Text = "« Adaptador de Red: Deshabilitado »";
+            //    notifyIcon.ShowBalloonTip(500);
+            //}
+        }
+
+        private void button1_Leave(object sender, EventArgs e)
+        {
+            Console.WriteLine("sali del boton, puto");
+        }
+
+        private void button1_Enter(object sender, EventArgs e)
+        {
+            Console.WriteLine("entré al boton, puto");
+        }
     }
 
 }
