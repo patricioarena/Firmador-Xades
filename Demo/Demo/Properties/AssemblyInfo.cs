eca9a97926ddc4c473489ed4bbe7b4fb710205db
Patricio Arena <parena@PARENA>
2019-12-09 13:40:12 -0300

    CertiFisc 1.0.0.0 versión publicada 09/12/2019 13:40

--- Demo/Demo/Properties/AssemblyInfo.cs
+++ Demo/Demo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Signature")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CertiFisc")]
+[assembly: AssemblyDescription("Aplicación para firmar digitalmente documentos utilizando estandar XadES")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Signature")]
+[assembly: AssemblyCompany("Fiscalia de Estado de la Provincia de Buenos Aires")]
+[assembly: AssemblyProduct("CertiFisc Firma Digital")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
