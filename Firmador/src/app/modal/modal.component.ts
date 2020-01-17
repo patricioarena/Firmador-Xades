7a43d9fca326258c7c85acd36d683e7368d8e4c7
Patricio Arena <parena@PARENA>
2020-01-17 14:54:12 -0300

    Falta cambiar tamaño de modal

---
+++ Firmador/src/app/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+
+  title: String;
+  data: any = [];
+  
+  constructor(
+    public modalRef: BsModalRef
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
