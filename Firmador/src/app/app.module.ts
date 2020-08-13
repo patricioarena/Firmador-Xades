7bdb1c37d335fce52c938447c2fef001012749f5
Patricio Arena <patricio.e.arena@gmail.com>
2020-08-13 16:23:32 -0300

    update: angular  8.3.29 a 9.1.0 (ng2-file-upload actualizado tambien)

--- Firmador/src/app/app.module.ts
+++ Firmador/src/app/app.module.ts
@@ -1,33 +1,30 @@
-import { BrowserModule, Title } from '@angular/platform-browser';
-import { NgModule, Component } from '@angular/core';
+import { Component, NgModule } from "@angular/core";
+import { BrowserModule, Title } from "@angular/platform-browser";
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { ModalModule } from "ngx-bootstrap/modal";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
 
-import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
-import { FormsModule } from '@angular/forms';
-import { FileUploadModule } from 'ng2-file-upload';
+import { FormsModule } from "@angular/forms";
+import { FileUploadModule } from "ng2-file-upload";
 
-import { DigitalSignatureService } from './service/digital-signature.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
+// import { DigitalSignatureService } from './service/digital-signature.service';
 
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { NgxSpinnerService } from 'ngx-spinner';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
-import { NotificationService } from './service/notification.service';
-import { RoutingModule } from './modules/routing.module';
-import { TitleService } from './service/title.service';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { ClipboardModule } from 'ngx-clipboard';
-import { ModalComponent } from './modal/modal.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClipboardModule } from "ngx-clipboard";
+import { ToastrModule } from "ngx-toastr";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { ModalComponent } from "./modal/modal.component";
+import { RoutingModule } from "./modules/routing.module";
+import { NotificationService } from "./service/notification.service";
+import { TitleService } from "./service/title.service";
 
 @NgModule({
   declarations: [
@@ -36,7 +33,7 @@ import { ModalComponent } from './modal/modal.component';
     HomeComponent,
     FooterComponent,
     HeaderComponent,
-    ModalComponent
+    ModalComponent,
   ],
   imports: [
     ClipboardModule,
@@ -49,12 +46,12 @@ import { ModalComponent } from './modal/modal.component';
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
-    NgxSpinnerModule,
     TooltipModule.forRoot(),
-    RoutingModule
+    RoutingModule,
   ],
-  providers: [ DigitalSignatureService, NgxSpinnerService, NotificationService, TitleService ],
+  // providers: [DigitalSignatureService, NgxSpinnerService, NotificationService, TitleService],
+  providers: [NotificationService, TitleService],
   bootstrap: [AppComponent],
-  entryComponents: [ ModalComponent ]
+  entryComponents: [ModalComponent],
 })
 export class AppModule { }
