2036f05536fe3bc2ab617b6d0ba51269ce89e19c
Ezequiel Merino Spinedi <emerinospinedi@fepba.gov.ar>
2025-03-18 20:21:14 -0300

    Fix

--- Demo/Demo/Utils/CertificateUtils.cs
+++ Demo/Demo/Utils/CertificateUtils.cs
@@ -18,14 +18,6 @@ namespace Demo.Utils
         [DllImport("user32.dll")]
         private static extern bool SetForegroundWindow(IntPtr hWnd);
 
-        [DllImport("user32.dll")]
-        private static extern bool IsIconic(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        private const int SW_RESTORE = 9;
-
         private static readonly string CertificateKey = Properties.Settings.Default.CertificateKey;
         private static readonly string CertificateName = Properties.Settings.Default.CertificateName;
         private static readonly string AuthenticationServer = Properties.Settings.Default.AuthenticationServer;
@@ -132,16 +124,20 @@ namespace Demo.Utils
                 {
                     title = "Seleccione un certificado.";
                 }
-                // Asegurar que la ventana del dialogo esté al frente
+
                 IntPtr windowHandle = Process.GetCurrentProcess().MainWindowHandle;
-                ShowWindow(windowHandle, SW_RESTORE);
-                SetForegroundWindow(windowHandle);
+                if(windowHandle != IntPtr.Zero)
+                {
+                    SetForegroundWindow(windowHandle);
+                }
+
                 X509Certificate2Collection scollection = X509Certificate2UI.SelectFromCollection(
                     fcollection,
                     title,
                     message,
                     X509SelectionFlag.SingleSelection
                 );
+
                 if (scollection != null && scollection.Count == 1)
                 {
                     cert = scollection[0];
